132cc1ef0e0bf44a7be1a46c127c478b63012eec
DENIS Séverine <severine.denis@csem.ch>
2020-05-30 21:56:01 +0200

    add missing pidv3_axi_conf.* files

---
+++ pidv3_axi_conf.c
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2019  OscillatorIMP Digital
+ * Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
+ */
+
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+/* memory management */
+#include <sys/mman.h>
+#include <pidv3_axi_core/pidv3_axi_config.h>
+#include <pidv3_axi_conf.h>
+
+int pidv3_axi_full_conf(const char *filename,
+	const int32_t kp_val, const int32_t ki_val, const int32_t kd_val,
+	const int32_t setpoint_val,
+	const int8_t int_rst_val, const int8_t sign_val, const enum input_set_t input_val)
+{
+
+	int retval = EXIT_FAILURE;
+	int fd = open(filename, O_RDWR);
+	if (fd < 0) {
+		printf("erreur d'ouverture de %s\n", filename);
+		return EXIT_FAILURE;
+	}
+
+	/* configure kxx */
+	if (ioctl(fd, PIDV3_AXI_SET(REG_PIDV3_AXI_KP), &kp_val) < 0)
+		goto fd_close;
+	if (ioctl(fd, PIDV3_AXI_SET(REG_PIDV3_AXI_KI), &ki_val) < 0)
+		goto fd_close;
+	if (ioctl(fd, PIDV3_AXI_SET(REG_PIDV3_AXI_KD), &kd_val) < 0)
+		goto fd_close;
+	/* configure setpoint */
+	if (ioctl(fd, PIDV3_AXI_SET(REG_PIDV3_AXI_SETPOINT), &setpoint_val) < 0)
+		goto fd_close;
+	/* configure int rst */
+	if (ioctl(fd, PIDV3_AXI_SET(REG_PIDV3_AXI_INT_RST), &int_rst_val) < 0)
+		goto fd_close;
+	/* configure sign */
+	if (ioctl(fd, PIDV3_AXI_SET(REG_PIDV3_AXI_SIGN), &sign_val) < 0)
+		goto fd_close;
+	/* configure if sofware or input */
+	if (ioctl(fd, PIDV3_AXI_SET(REG_PIDV3_AXI_INPUT), &input_val) < 0)
+		goto fd_close;
+
+	retval = EXIT_SUCCESS;
+fd_close:
+	close(fd);
+	return retval;
+}
+
+int __pidv3_axi_set(const char *filename, const int32_t reg, const int32_t val)
+{
+	int retval = EXIT_FAILURE;
+	int fd = open(filename, O_RDWR);
+	if (fd < 0) {
+		printf("erreur d'ouverture de %s\n", filename);
+		return EXIT_FAILURE;
+	}
+
+	if (ioctl(fd, PIDV3_AXI_SET(reg), &val) < 0)
+		goto fd_close;
+
+	retval = EXIT_SUCCESS;
+fd_close:
+	close(fd);
+	return retval;
+}
+
+int pidv3_axi_set_kchan(const char *filename, const enum k_chan_t chan, const int32_t val)
+{
+	int reg = (chan == KP) ? REG_PIDV3_AXI_KP : ((chan == KI) ? REG_PIDV3_AXI_KI:REG_PIDV3_AXI_KD);
+	return __pidv3_axi_set(filename, reg, val);
+}
+
+int pidv3_axi_set_setpoint(const char *filename, const int32_t val)
+{
+	return __pidv3_axi_set(filename, REG_PIDV3_AXI_SETPOINT, val);
+}
+
+int pidv3_axi_set_int_rst(const char *filename, const int8_t val)
+{
+	return __pidv3_axi_set(filename, REG_PIDV3_AXI_INT_RST, val);
+}
+
+int pidv3_axi_set_sign(const char *filename, const int8_t val)
+{
+	return __pidv3_axi_set(filename, REG_PIDV3_AXI_SIGN, val);
+}
+
+int pidv3_axi_set_sw_hw(const char *filename, const enum input_set_t input)
+{
+	return __pidv3_axi_set(filename, REG_PIDV3_AXI_INPUT, input);
+}
---
+++ pidv3_axi_conf.h
@@ -0,0 +1,86 @@
+#ifndef PIDV3_AXI_CONF_H
+#define PIDV3_AXI_CONF_H
+
+#ifdef __cplusplus /* if C++, specify external linkage to C functions */
+extern "C" {
+#endif
+#include <stdint.h>
+
+#include <pidv3_axi_core/pidv3_axi_config.h>
+
+enum k_chan_t {
+	KP = 0,
+	KI,
+	KD
+};
+
+enum input_set_t {
+	INPUT_SETPOINT_SW= PIDV3_AXI_SETPOINT_SW,
+	INPUT_SETPOINT_HW= PIDV3_AXI_SETPOINT_HW,
+	INPUT_KP_SW      = PIDV3_AXI_KP_SW,
+	INPUT_KP_HW      = PIDV3_AXI_KP_HW,
+	INPUT_KI_SW      = PIDV3_AXI_KI_SW,
+	INPUT_KI_HW      = PIDV3_AXI_KI_HW,
+	INPUT_KD_SW      = PIDV3_AXI_KD_SW,
+	INPUT_KD_HW      = PIDV3_AXI_KD_HW,
+	INPUT_SIGN_SW    = PIDV3_AXI_SIGN_SW,
+	INPUT_SIGN_HW    = PIDV3_AXI_SIGN_HW,
+	INPUT_INT_RST_SW = PIDV3_AXI_INT_RST_SW,
+	INPUT_INT_RST_HW = PIDV3_AXI_INT_RST_HW,
+	INPUT_ALL_HW     = PIDV3_AXI_SETPOINT_HW |
+						PIDV3_AXI_KP_HW |
+						PIDV3_AXI_KI_HW |
+						PIDV3_AXI_KD_HW |
+						PIDV3_AXI_SIGN_HW |
+						PIDV3_AXI_INT_RST_HW,
+	INPUT_ALL_SW     = PIDV3_AXI_SETPOINT_SW |
+						PIDV3_AXI_KP_SW |
+						PIDV3_AXI_KI_SW |
+						PIDV3_AXI_KD_SW |
+						PIDV3_AXI_SIGN_SW |
+						PIDV3_AXI_INT_RST_SW
+};
+
+/* configure all parameters at the same time
+ * chanA_val/chanB_val: output value
+ * enable_high: set if output enable signal is always high (1) or if
+ *	it used in oneshot mode (0)
+ * sync_chan: set to 0 for independent access to chanA and chanB, set to 1 to
+ * 	update chanA and chanB in a same time
+ */
+int pidv3_axi_full_conf(const char *filename,
+	const int32_t kp_val, const int32_t ki_val, const int32_t kd_val,
+	const int32_t setpoint_val,
+	const int8_t int_rst, const int8_t sign, const enum input_set_t input);
+
+/* send chan value:
+ * chan: must be KP, KI or KD
+ * value: value to send
+ */
+int pidv3_axi_set_kchan(const char *filename, const enum k_chan_t chan, const int32_t val);
+
+/* send setpoint value:
+ * value: value to send
+ */
+int pidv3_axi_set_setpoint(const char *filename, const int32_t val);
+
+/* send int_rst:
+ * value: value to send
+ */
+int pidv3_axi_set_int_rst(const char *filename, const int8_t val);
+
+/* send sign
+ * value: value to send
+ */
+int pidv3_axi_sign(const char *filename, const int8_t val);
+
+/* configure if signal is from AXI or from input
+ * input: configuration
+ */
+int pidv3_axi_set_sw_hw(const char *filename, const enum input_set_t input);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*PIDV3_AXI_CONF_H*/
